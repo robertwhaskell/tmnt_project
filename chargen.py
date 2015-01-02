7516a9a1fa00f8e41091b60526fc83c159b4a6d0
Robert Haskell <robertwhaskell@gmail.com>
2015-01-02 03:22:20 -0800

    clear

--- chargen.py
+++ chargen.py
@@ -9,11 +9,33 @@ def Make_Powers_Array():
         power_array.append(Attack_Power(power[0], power[1], power[2]))
       elif len(power) == 4:
         power_array.append(Defense_Power(power[0], power[1], power[2], power[3]))
-  return 0
+  return power_array
 
 def Make_Animals_Array():
   animals_array = []
-  with open("animalnames.txt", "r")
+  with open("animalnames.txt", "r") as animals:
+    line_counter = 0
+    name = ""
+    bioe = 0
+    sl = 0
+    bonuses = []
+    powers = []
+    for line in animals.readlines():
+      if line_counter == 5:
+        animals_array.append(Animal(name, bioe, sl, bonuses, powers))
+        line_counter = 0
+      if line_counter == 0:
+        name = line.strip()
+      elif line_counter == 1:
+        bioe = int(line)
+      elif line_counter == 2:
+        sl = int(line)
+      elif line_counter == 3:
+        bonuses = line.split()
+      elif line_counter == 4:
+        powers = line.split()
+      line_counter += 1
+  return animals_array
 
 class Attack_Power(object):
   def __init__(self, name, cost, attack_die):
@@ -33,12 +55,22 @@ class Other_Power(object):
     self.name = name
     self.cost = cost
 
-
-
 class Animal(object):
+  def str(self):
+    print self.name
+    print self.bioe
+    print self.sl
+    print self. bonuses
+    print self.powers
   def __init__(self, name, bioe, sl, bonuses, powers):
     self.name = name
     self.bioe = bioe
     self.sl = sl
     self.bonuses = bonuses
     self.powers = powers
+
+powers = Make_Powers_Array()
+animals = Make_Animals_Array()
+for an in animals:
+  print an.str()
+  print ""
